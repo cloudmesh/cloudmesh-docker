0b7073d84448753f446e3f5674d558c312d0a14a
Gregor von Laszewski <laszewski@gmail.com>
2019-10-23 16:43:24 -0400

    create wrapper tamplate for docker cms container

--- cloudmesh/docker/shell.py
+++ cloudmesh/docker/shell.py
@@ -1,47 +1,18 @@
 from __future__ import print_function
 
-import importlib
-import inspect
-import os
-import pkgutil
-import pydoc
-import sys
-import textwrap
-import subprocess
-import shlex
-from docopt import docopt
-
 from pprint import pprint
-from cmd import Cmd
 
-from cloudmesh.common.Printer import Printer
-from cloudmesh.common.Shell import Shell
-from cloudmesh.common.StopWatch import StopWatch
 from cloudmesh.common.dotdict import dotdict
-from cloudmesh.common.util import path_expand
-from cloudmesh.common.default import Default
-from cloudmesh.common.error import Error
-from cloudmesh.common.console import Console
-from cloudmesh.common.util import readfile
-
-import cloudmesh
-import cloudmesh.common
-from cloudmesh.shell.command import PluginCommand, map_parameters
-from cloudmesh.shell.command import command, basecommand
-from cloudmesh.shell.plugin import PluginManager
-from cloudmesh.common.variables import Variables
-from cloudmesh.common.debug import VERBOSE
-
-
-# import cloudmesh.plugin
-
-
+from cloudmesh.shell.command import map_parameters
+from docopt import docopt
 
 
 def main():
     """cms.
 
     Usage:
+      cm install
+      cm init
       cm --help
       cm [--echo] [--debug] [--nosplash] [-i] [COMMAND ...]
 
@@ -55,10 +26,9 @@ def main():
       --nosplash    do not show the banner [default: False]
     """
 
-    args = sys.argv[1:]
+    arguments = dotdict(docopt(str(main.__doc__)))
 
-    arguments = dotdict(docopt(main.__doc__))
-
+    pprint(arguments)
     map_parameters(arguments,
                    'debug',
                    'echo',
@@ -72,7 +42,8 @@ def main():
     command = ' '.join(arguments.COMMAND)
 
     execute = f"docker run cms {options} {command}"
-    print (execute)
+    print(execute)
+
 
 if __name__ == '__main__':
     main()
